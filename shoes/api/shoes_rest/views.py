97959fa8c446922357a601a62ed18aa0061910cd
Mary McKinnon <your@email.heremaryamckinnon@gmail.com>
2022-06-15 13:28:35 -0700

    Created models and views for shoes

--- shoes/api/shoes_rest/views.py
+++ shoes/api/shoes_rest/views.py
@@ -1,3 +1,68 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+from common.json import ModelEncoder
+from .models import BinVO, Shoe
+import json
+
 
 # Create your views here.
+class BinVODetailEncoder(ModelEncoder):
+    model = BinVO
+    properties = ["closet_name", "bin_number", "bin_size"]
+
+    
+class ShoeListEncoder(ModelEncoder):
+    model = Shoe
+    properties = ["model_name"]
+
+    def get_extra_data(self, o):
+        return {"bin": o.bin.bin_number}
+
+
+class ShoeDetailEncoder(ModelEncoder):
+    model = Shoe
+    properties = ["manufacturer", "model_name", "color"]
+    encoders = {
+        "bin": BinVODetailEncoder(),
+    }
+
+
+@require_http_methods(["GET", "POST"])
+def api_list_shoes(request):
+    if request.method == "GET":
+        shoe = Shoe.objects.all()
+        return JsonResponse(
+            {"shoe": shoe},
+            encoder=ShoeListEncoder,
+        )
+
+    else:
+        content = json.loads(request.body)
+
+        shoe = Shoe.objects.create(**content)
+        return JsonResponse(
+            shoe,
+            encoder=ShoeListEncoder,
+            safe=False,
+        )
+
+
+@require_http_methods(["GET", "PUT", "DELETE"])
+def api_show_shoes(request, pk):
+    if request.method == "GET":
+        shoe = Shoe.objects.get(id=pk)
+        return JsonResponse(
+            shoe,
+            encoder=ShoeDetailEncoder,
+            safe=False,
+        )
+    elif request.method == "PUT":
+        content = json.loads(request.body)
+        Shoe.objects.filter(id=pk).update(**content)
+        shoe = Shoe.objects.get(id=pk)
+        return JsonResponse(
+            shoe,
+            encoder=ShoeDetailEncoder,
+            safe=False,
+        )
