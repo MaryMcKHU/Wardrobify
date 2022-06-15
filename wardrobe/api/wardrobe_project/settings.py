3df6ba1db24b7645a421b52428918c671f2d0ec2
Mary McKinnon <your@email.heremaryamckinnon@gmail.com>
2022-06-15 11:28:40 -0700

    Made changes to settings to add apps

--- wardrobe/api/wardrobe_project/settings.py
+++ wardrobe/api/wardrobe_project/settings.py
@@ -26,12 +26,14 @@ SECRET_KEY = 'django-insecure-e9kf6nups(2tagv+$@pg(p^*4cz#tgffqchjnxqw1c*t7yfauc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    "hats.apps.HatsConfig",
+    "shoes.apps.ShoesConfig",
     "corsheaders",
     "wardrobe_api.apps.WardrobeApiConfig",
     'django.contrib.admin',
